c106423a3ea713131ae659ff994dfc42b9504ab7
Hardik-Dulani <hardikdulani66@gmail.com>
2024-08-08 11:15:23 +0530

    information added

--- pages/about.py
+++ pages/about.py
@@ -1,103 +1,59 @@
-# def main():
-#     import streamlit as st
-#     import pickle
-#     import helper
-#     my_data = {}
-#     descriptions = {}
-#     data = {}
+import streamlit as st
 
-#     # Full forms and descriptions of each point
-#     descriptions = {
-#         'Name': "Full name of the patient",
-#         'Sex': "Sex of the patient",
-#         'chest_pain': """ 
-#         - **Typical chest pain** feels like pressure or squeezing in the center of the chest, often worsening with activity or stress. It might spread to the neck, jaw, arms, or back and can be relieved by rest or medication. \n 
-#         - **Atypical chest pain**, on the other hand, can be sharp, stabbing, or burning, and can occur anywhere in the chest. It's not always related to physical exertion and may be triggered by movements, breathing, or eating. Atypical pain can be relieved by antacids or changing positions and is less likely to spread to other parts of the body.""",
-        
-#         'EF-TTE': "Ejection Fraction from Transthoracic Echocardiogram (% range: 0-100)",
-#         'Age': "Age of the patient (years)",
-#         'Region RWMA': "Regional Wall Motion Abnormality (0: No, 1: Yes)",
-#         'HTN': "Hypertension (0: No, 1: Yes)",
-#         'BP': "Blood Pressure (mm Hg range: 80-200)",
-#         'ESR': "Erythrocyte Sedimentation Rate (mm/hr range: 0-100)",
-#         'FBS': "Fasting Blood Sugar (mg/dL range: 70-130)",
-#         'Nonanginal': "Non-anginal chest pain (0: No, 1: Yes)",
-#         'DM': "Diabetes Mellitus (0: No, 1: Yes)",
-#         'Tinversion': "T wave inversion in ECG (0: No, 1: Yes)",
-#         'K': "Potassium level in blood (mEq/L range: 3.5-5.5)",
-#         'TG': "Triglycerides (mg/dL range: 0-150)",
-#         'Na': "Sodium level in blood (mEq/L range: 135-145)",
-#         'Length': "Height of the patient (cm range: 50-250)",
-#         'Weight': "Weight of the patient (kg range: 10-200)",
-#         'Lymph': "Lymphocyte count (cells/uL range: 1000-4000)",
-#         'PLT': "Platelet count (cells/uL range: 150,000-450,000)",
-#         'Dyspnea': "Shortness of breath (0: No, 1: Yes)",
-#         'BMI': "Body Mass Index (kg/m² range: 10-50)"
-#     }
+# Title of your Streamlit app
+st.title("Coronary Artery Disease Prediction Project")
 
-#     # Function to calculate BMI
+# Introduction section
+st.header("Introduction")
+st.write("""
+Our goal was to develop a model for predicting coronary artery disease (CAD) and to compare the performance of various machine learning algorithms. Additionally, we aimed to analyze the explainability of the chosen machine learning algorithm.
+""")
 
+# Dataset Description section
+st.header("Dataset Description")
+st.subheader("Z-Alizadeh Sani Dataset")
+st.markdown("""
+The **Z-Alizadeh Sani dataset** is available on the [UCI Machine Learning repository](https://doi.org/10.24432/C5Q31T). It comprises 303 medical records from patients who visited Shaheed Rajaei hospital in Iran due to chest pain. Each record contains 55 features, categorized into:
 
-#     # Setting the title
-#     # st.markdown("<h1 style='font-size: 36px;'>Medical Information </h1>", unsafe_allow_html=True)
-#     st.write("We're collecting basic information needed for our model to work. If you have this information, please provide it. If not, simply select 'NA' or leave blank for numerical inputs and we will use the most probable input based on our training data to give predictions.")
+1. **Demographic Features**
+2. **Symptoms and Physical Examination**
+3. **ECG (Electrocardiogram)**
+4. **Echocardiography Features**
 
-#     # Creating the form
+### Class Distribution
+- **CAD Class**: 216 instances (71.29%)
+- **Normal Class**: 87 instances (28.71%)
 
-#     with st.form(key='medical_info_form'):
-#         name = st.text_input("**Name:**", help=descriptions['Name'])
-#         my_data['Sex']=st.selectbox("**Sex:**", ["Male","Female","Other"], help=descriptions['Sex'])
-#         my_data['Age'] = st.number_input("**Age (years):**", min_value=12, max_value=120, help=descriptions['Age'])
-#         my_data['Weight'] = st.number_input("**Weight (kg):**", min_value=15.0, max_value=250.0, step=0.5 ,help=descriptions['Weight'])
-#         my_data['Length'] = st.number_input("**Height (Length) (cm):**", min_value=50.0, max_value=250.0, step = 0.5, help=descriptions['Length'])
-#         chest_pain = st.selectbox("**Chest Pain:**", ["Typical", "Atypical","None"], help=descriptions["chest_pain"])
-#         smoker = st.selectbox("**Smoking**", ["Never", "Currently active","in the past"], help=descriptions["chest_pain"])
-#         # my_data['Atypical'] = st.selectbox("**Atypical Chest Pain:**", ["NA","No", "Yes"], help=descriptions['Atypical'])
-#         my_data['Region RWMA'] =st.selectbox("**Region RWMA:**", ["NA","0 (Normal)", "1 (Hypokinesis)", "2 (Akinesis)", "3 (Dyskinesis)", "4 (Aneurysmal)"], help=descriptions['Region RWMA'])
-#         my_data['HTN'] = st.selectbox("**Hypertension (HTN):**", ["NA","No", "Yes"], help=descriptions['HTN'])
-#         my_data['Nonanginal'] = st.selectbox("**Nonanginal Chest Pain:**", ["NA","No", "Yes"], help=descriptions['Nonanginal'])
-#         my_data['DM'] = st.selectbox("**Diabetes Mellitus (DM):**", ["NA","No", "Yes"], help=descriptions['DM'])
-#         my_data['Tinversion'] = st.selectbox("**T wave Inversion (Tinversion):**", ["NA","No", "Yes"], help=descriptions['Tinversion'])
-#         my_data['Dyspnea'] = st.selectbox("**Dyspnea (Shortness of Breath):**", ["NA","No", "Yes"], help=descriptions['Dyspnea'])
-#         my_data['EF-TTE'] = st.text_input("**EF-TTE (%):**", help=descriptions['EF-TTE'])
-#         my_data['BP'] = st.text_input("**Blood Pressure (BP) (mm Hg):**",  help=descriptions['BP'])
-#         my_data['ESR'] = st.text_input("**Erythrocyte Sedimentation Rate (ESR) (mm/hr):**", help=descriptions['ESR'])
-#         my_data['FBS'] = st.text_input("**Fasting Blood Sugar (FBS) (mg/dL):**", help=descriptions['FBS'])
-#         my_data['K'] = st.text_input("**Potassium Level (K) (mEq/L):**",  help=descriptions['K'])
-#         my_data['TG'] = st.text_input("**Triglycerides (TG) (mg/dL):**", help=descriptions['TG'])
-#         my_data['Na'] = st.text_input("**Sodium Level (Na) (mEq/L):**",  help=descriptions['Na'])
-#         my_data['Lymph'] = st.text_input("**Lymphocyte Count (cells/uL):**",  help=descriptions['Lymph'])
-#         my_data['PLT'] = st.text_input("**Platelet Count (PLT) (cells/uL):**",  help=descriptions['PLT'])
-        
-#         # Automatically calculate BMI
-#         my_data['BMI'] = helper.calculate_bmi(my_data['Weight'], my_data['Length'])
-#         # st.write(f"**Body Mass Index (BMI):** {bmi_value:.2f} kg/m²")
-#         my_data['Typical Chest Pain'] =  int(chest_pain == 'Typical')
-#         my_data['Atypical'] =  int(chest_pain == 'Atypical')
-#         my_data['Current Smoker'] = int(smoker == 'Currently active')
-#         my_data['EX-Smoker'] = int(smoker == 'in the past')
-#         # Submit button
-#         submit_button = st.form_submit_button(label='Submit')
+A sample is classified as CAD if the stenosis of coronary arteries lumen reaches or exceeds 50%; otherwise, it is classified as normal. Both ECG and echocardiography features are obtained by professional doctors.
+""")
 
-#     # Handling form submission
-#     if submit_button:
-#         file_path = 'defaults.pkl'
+# Exploratory Data Analysis section
+st.header("Exploratory Data Analysis")
+st.write("""
+In this part, we analyzed the data distribution and examined the basic statistical properties of the data. This included understanding the types of variables, checking for missing values, and visualizing the distribution of various features and the target variable. This step helped us identify trends, anomalies, patterns, and relationships within the data.
+""")
 
-#     # Load the pickle file
-#         data = {}
-#         with open(file_path, 'rb') as file:
-#             defaults = pickle.load(file)
-#         for i,j in defaults.items():
-#             if i not in my_data or my_data[i] == '' or my_data[i] == None or  my_data[i] == 'NA':
-#                 data[i] = j
+# Predictive Analysis section
+st.header("Predictive Analysis")
+st.write("""
+We processed the data by splitting it into training and test sets, and oversampled the data using SMOTENC. We then developed predictive models with hyperparameter tuning using Optuna.
 
-#             else:
-#                 if i == 'Region RWMA':
-#                     data[i] = int(my_data[i][0])
-#                 else:
-#                     data[i] = my_data[i]
+We built nine models including: CatBoost, XGBoost, LightGBM, and three ensemble models.  We compared these models using various metrics and selected CatBoost for further explainability analysis.
 
-            
-#         st.dataframe(data)
-# if __name__ == '__main__':
-#     main()
+CatBoost, Ensemble3, XGBoost, and LightGBM achieved 100% recall, meaning all CAD patients were correctly identified as CAD class by these models. Since predicting CAD class as normal class is dangerous, reducing false negatives is crucial. CatBoost and Ensemble3 stood out with 91.8% accuracy and a 0.945 F1 score and thus we selected **CatBoost** for further explainability analysis and deployment.
+""")
+
+# Explainable AI using SHAP section
+st.header("Explainable AI Using SHAP")
+st.write("""
+We interpreted the CatBoost model using SHAP values. By plotting Feature Importance, Summary, and Dependence plots, we gained insights into the global interpretability of the model.
+
+For local interpretability, we analyzed specific cases, such as Patient No. 77 and Patient No. 7 from the training dataset. Using SHAP Waterfall and Force plots, along with current clinical guidelines, we made recommendations for coronary angiography.
+
+Explainable AI (XAI) methods like SHAP, LIME, ELI5, CAM, and Grad-CAM help reduce human errors in the medical field. Our model, CatBoost with SHAP, aligns well with current decision-making processes and can significantly aid in making accurate and efficient decisions. This method is applicable to other medical datasets and can be utilized across various areas of the medical field.
+""")
+
+# Footer or additional information
+st.markdown("---")
+st.write("For more information, visit the [UCI Machine Learning repository](https://doi.org/10.24432/C5Q31T) to explore the Z-Alizadeh Sani dataset in detail.")
+st.write("We have documented our findings and methodology in a research paper, which can be accessed [here](https://doi.org/10.1007/978-3-031-65392-6_24)")
